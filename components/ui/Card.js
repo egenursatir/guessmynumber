dd7fe9b221054d0f772af47beeb7a716e26bd127
egenursatir <egenursatir@gmail.com>
2023-03-02 12:56:13 -0500

    Add IOS compatibility

---
+++ components/ui/Card.js
@@ -0,0 +1,25 @@
+import {StyleSheet, View} from 'react-native';
+import Colors from '../../constants/colors';
+
+function Card({children}) {
+    return (
+        <View style={styles.card}>
+            {children}
+        </View>
+    )
+}
+
+export default Card;
+
+const styles=StyleSheet.create({
+    card: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        backgroundColor: Colors.primary800,
+        elevation: 8
+    },
+})
---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    primary500: '#72063c',
+    primary600: '#640233',
+    accent500: '#ddb52f',
+    primary700: '#4e0329',
+    primary800: '#3b021f'
+}
+
+export default Colors;
